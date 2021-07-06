f3918b3c3afc33f60147540442d319fb2bec2970
yabing.dong <yabing.dong@analyticservice.net>
2021-07-06 16:34:04 +0800

    fix

--- app.js
+++ app.js
@@ -5,18 +5,12 @@ App({
     const logs = wx.getStorageSync('logs') || []
     logs.unshift(Date.now())
     wx.setStorageSync('logs', logs)
+    wx.setStorageSync('role', 'admin')
 
     // 登录
     wx.login({
       success: res => {
         // 发送 res.code 到后台换取 openId, sessionKey, unionId
-        let random = Math.random()
-        console.log('random', random)
-        if (random > 0.5) {
-          wx.setStorageSync('role', 'admin')
-        } else {
-          wx.setStorageSync('role', 'visitor')
-        }
       }
     })
   },
